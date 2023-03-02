f1ce436f9fae7157e4601317a0f3aad474dec71e
Pavla Zimm <zimmermannovap@gmail.com>
2023-03-02 14:44:20 +0100

    second3

--- main.js
+++ main.js
@@ -4,9 +4,11 @@
 
 let celeJmeno = prompt("Jaké je vaše jméno a přijmení?");
 let vek = prompt("Jaký je váš rok narození?");
-let barvaZadanaUzivatelem = prompt("napište vaši oblíbenou barvu anglicky?")
-
-
-
+let barvaZadanaUzivatelem = prompt("napište vaši oblíbenou barvu anglicky?");
+git add .
 document.body.innerHTML += "<p>" + celeJmeno + ", věk: " + (2023-vek) + "</p>";
-odstavec.style.color = barvaZadanaUzivatelem;
+
+function zmenaBarvy(){
+    let text = document.querySelector('.odstavec');
+    ctverecek.style.backgroundColor = "barvaZadanaUzivatelem";
+  }
