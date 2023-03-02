214357b38b8f82a038d9a7fbd0efce94cd620725
Pavla Zimm <zimmermannovap@gmail.com>
2023-03-02 14:28:35 +0100

    second

--- main.js
+++ main.js
@@ -1,13 +1,12 @@
 // tady je místo pro náš program
 
 
+
 let celeJmeno = prompt("Jaké je vaše jméno a přijmení?");
 let vek = prompt("Jaký je váš rok narození?");
+let barvaZadanaUzivatelem = prompt("napište vaši oblíbenou barvu anglicky?Pavla")
+
+
 
 document.body.innerHTML += "<p>" + celeJmeno + ", věk: " + (2023-vek) + "</p>";
-
-
-
-
-
-
+odstavec.style.color = barvaZadanaUzivatelem;e
